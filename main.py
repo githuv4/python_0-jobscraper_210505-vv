d385e5fd496619cc113f09d779ce96c862ac47a0
gitsmin <sendex485@gmail.com>
2021-05-01 22:57:31 +0000

    2.4 extracting pages

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
 indeed_result = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit=50&start=0")
 
-# print(indeed_result)
-print(indeed_result.text)
+indeed_soup = BeautifulSoup(indeed_result.text,"html.parser")
+
+pagination = indeed_soup.find("div",{"class":"pagination"})
+links = pagination.find_all("a")
+
+spans = []
+
+for link in links[:-1]:
+  spans.append(int(link.find("span").string))
+last_page = spans[-1]
+print(spans)
+print(last_page)
+
+
