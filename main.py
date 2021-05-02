946a98b749ed4d00aec2585f5629540e632de6b0
gitsmin <sendex485@gmail.com>
2021-05-02 20:49:15 +0000

    missing 2.5

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from indeed import extract_indeed_pages, extract_indded_jobs
 
 
-
 last_indeed_page = extract_indeed_pages()
-extract_indeed_jobs = extract_indded_jobs(last_indeed_page)
-
+indeed_jobs = extract_indded_jobs(last_indeed_page)
