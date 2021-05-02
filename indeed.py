215b2969655704da0021a260c9e0dd659c4c59da
gitsmin <sendex485@gmail.com>
2021-05-02 06:16:11 +0000

    2.5 requesting each page 2

--- indeed.py
+++ indeed.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 LIMIT=50
-LAST_PAGE = 10
-URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={LIMIT}&"
+LAST_PAGE = 3
+URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={LIMIT}"
 
 def extract_indeed_pages():  
   indeed_result = requests.get(URL)
@@ -20,10 +20,14 @@ def extract_indeed_pages():
 
   return last_page
   
-
-def extract_indded_jobs(last_page):
-  for page in range(last_page):
-    print (f"&start={page*LIMIT}")
-
   # range(s,n)   : 숫자n부터 시작하는 n개 수모임
-  # range(0,5)   : 0부터시작하는 5개 : 0 1 2 3 4
+  # range(0,5)   : 0부터시작하는 5개
+  # 0 1 2 3 4
+def extract_indded_jobs(last_page):
+  jobs=[]
+  for page in range(last_page):
+    result = requests.get(f"{URL}&start={page*LIMIT}")
+    # print(result.status.code)
+    print(result.status_code)
+  return jobs
+  
